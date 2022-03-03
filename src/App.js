4c1ef7df3b62c34348377383aa3c32219627f646
marcelomrad <marcelomrad@gmail.com>
2022-03-03 19:08:19 -0300

    MyAccount

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-// import {Routes,Route,Link} from 'react-router-dom'
-// import Pg1 from './pages/Login/Login'
-// import Pg2 from './pages/Cadastro/Cadastro'
+import {Routes,Route} from 'react-router-dom'
 import './App.css';
 import home from './images/home.svg';
 import games from './images/games.svg';
-import count from './images/count.svg';
+import account from './images/count.svg';
 import out from './images/out.svg';
 import Home from './pages/Home/Home';
+import MyAccount from './pages/MyAccount/MyAccount'
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import Button from 'react-bootstrap/Button'
@@ -21,16 +20,22 @@ export default function App() {
           <Navbar className="navbar, align-items-start">
             <Container>
             <Nav className="flex-column">
-              <Nav.Link href="#home"><img src={home} alt="home"></img></Nav.Link>
-              <Nav.Link href="#games"><img src={games} alt="games"></img></Nav.Link>
-              <Nav.Link href="#count"><img src={count} alt="count"></img></Nav.Link>
+              <Nav.Link href="/home" ><img src={home} alt="home"></img ></Nav.Link>
+              <Nav.Link href="/games"><img src={games} alt="games"></img></Nav.Link>
+              <Nav.Link href="/count" ><img src={account} alt="count"></img></Nav.Link>
               <Nav.Link className="xx" href="#out">
                 <img src={out} alt="out"/>
               </Nav.Link>
             </Nav>
             </Container>
           </Navbar>
-          <Home></Home>
+
+          <Routes >
+            <Route path='/count' element = {<MyAccount/>} />
+            <Route path='/home' element = {<Home/>} />
+          </Routes>
+
+          
       </div>
     {/* <Pg1></Pg1> */}
     </>
---
+++ src/pages/MyAccount/ModalSenha/ModalSenha.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./ModalSenha.css";
+
+export default function ModalEmail({ closeModal }) {
+  return (
+    <div className="modalBackground">
+      <div className="modalContainerSenha">
+        <div className="title">
+          <h2>Editar Email</h2>
+        </div>
+        <div className="body"></div>
+
+        <label className="labele">Senha Atual</label>
+        <input 
+            className="inpute" 
+            type="password" 
+            name="senhaAtual" />
+
+        <label className="labele">Nova Senha</label>
+        <input 
+            className="inpute" 
+            type="password" 
+            name="newSenha" />
+
+        <label className="labele">Confirmar nova senha</label>
+        <input 
+            className="inpute"
+            type="password" 
+            name="ConfirmarSenha" />
+
+        <div className="footer">
+          <button
+            onClick={() => {
+              closeModal(false);
+            }}
+            id="cancelButton"
+          >
+            Cancelar
+          </button>
+
+          <button>Confirmar</button>
+        </div>
+      </div>
+    </div>
+  );
+}
